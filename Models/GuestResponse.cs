1cde3e1dcb5198626c3d91eefa7af3487bcc29c5
Lukasz <spiderking@interia.pl>
2019-12-28 16:44:34 +0100

    secend commmit

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="prosze podac imie i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "prosze podac email")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage ="prosze podac prawidłowy adres email")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "prosze podac nr tel")]
+
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "prosze okreslic czy wezmiesz udzial")]
+
+        public bool? WillAttend { get; set; }
+    }
+}
